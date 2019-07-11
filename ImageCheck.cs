b93be0e2c08e86f0d8ca83f61e609819f5191faf
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2019-07-11 21:57:26 +0200

    Increased performance + avoid some errors with try/catch
    Added try/catch block to avoid wrong link
    Replaced var by its real type
    Replaced foreach loop to for loop

--- ImageCheck.cs
+++ ImageCheck.cs
@@ -2,6 +2,8 @@ using System;
 using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Security.AccessControl;
+using System.Security.Principal;
 
 namespace CheckURI.Tools
 {
@@ -12,19 +14,22 @@ namespace CheckURI.Tools
             string filter = media + "/";
             if (contentType != null) return contentType.StartsWith(filter);
             if (uri.Scheme == "data") return uri.ToString().Split(':')[1].ToLower().StartsWith(filter);
-
-            using (HttpRequestMessage req = new HttpRequestMessage(HttpMethod.Head, uri))
-            using (var response = await Downloader.client.SendAsync(req, HttpCompletionOption.ResponseHeadersRead))
+                
+            try
             {
-                if (response.IsSuccessStatusCode)
+                using (HttpRequestMessage req = new HttpRequestMessage(HttpMethod.Head, uri))
+                using (HttpResponseMessage response = await Downloader.client.SendAsync(req, HttpCompletionOption.ResponseHeadersRead))
                 {
-                    using (HttpContent content = response.Content)
+                    if (response.IsSuccessStatusCode)
                     {
-                        string mediaType = content.Headers.ContentType.MediaType;
-                        if (!String.IsNullOrEmpty(mediaType)) return mediaType.StartsWith(filter);
+                        using (HttpContent content = response.Content)
+                        {
+                            string mediaType = content.Headers.ContentType.MediaType;
+                            if (!String.IsNullOrEmpty(mediaType)) return mediaType.StartsWith(filter);
+                        }
                     }
                 }
-            }
+            } catch { }
             return false;
         }
     
@@ -37,9 +42,10 @@ namespace CheckURI.Tools
             AuthorizationRuleCollection rules = acl.GetAccessRules(true, true, typeof(SecurityIdentifier));
             if (rules == null) return false;
 
-            var identify = WindowsIdentity.GetCurrent();
-            foreach (AuthorizationRule rule in rules)
+            WindowsIdentity identify = WindowsIdentity.GetCurrent();
+            for (int i = 0, l = rules.Count; i < l; i++)
             {
+                AuthorizationRule rule = rules[i];
                 if (!(rule is FileSystemAccessRule fsAccessRule)) continue;
                 if (identify.Groups.Contains(rule.IdentityReference))
                 {
